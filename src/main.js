0f8109ae31a3fc402a24888b6c27e1cc6d0cb283
Ha Quanq <quangha0120010@gmail.com>
2025-07-29 08:05:13 +0700

    Refactor mobile menu control

--- src/main.js
+++ src/main.js
@@ -1,45 +1,40 @@
-const mobileMenu = {
-  menuElement: document.querySelector(".nav__link__list"),
-  menuButton: document.querySelector(".nav__menu__btn"),
-  isOpening: false,
-  showMenu() {
+const menuWrapper = document.querySelector(".nav__list");
+const menuButton = document.querySelector(".nav__menu-btn");
+
+let isMenuOpened = false;
+
+const showMenu = () => {
     window.scrollTo({ top: 0 });
-    this.isOpening = true;
-    this.menuElement.style.display = "block";
-    this.menuButton.classList.add("pressed");
+    isMenuOpened = true;
+    menuWrapper.style.display = "block";
+    menuButton.setAttribute("aria-expanded", "true");
     document.querySelector("body").style.overflow = "hidden";
-    document.querySelectorAll(".intro__wrapper a, button").forEach((el) => (el.tabIndex = "-1"));
-  },
-  hideMEnu() {
-    this.isOpening = false;
-    this.menuElement.style.display = null;
-    this.menuButton.classList.remove("pressed");
-    document.querySelector("body").style.overflow = null;
-    document.querySelectorAll(".intro__wrapper a, button").forEach((el) => (el.tabIndex = "0"));
-  },
-  init() {
-    this.menuButton.addEventListener("click", (e) => {
-      e.stopPropagation();
-      if (this.isOpening) this.hideMEnu();
-      else this.showMenu();
-    });
-
-    this.menuElement.addEventListener("click", (e) => {
-      e.stopPropagation();
-    });
-
-    window.addEventListener("click", () => {
-      if (this.isOpening) this.hideMEnu();
-    });
-
-    window.addEventListener("resize", (e) => {
-      if (this.isOpening && window.matchMedia("(min-width: 768px)")) this.hideMEnu();
-    });
-
-    window.addEventListener("keydown", (e) => {
-      if (e.key == "Escape" && this.isOpening) this.hideMEnu();
-    });
-  },
 };
 
-mobileMenu.init();
+const hideMenu = () => {
+    isMenuOpened = false;
+    menuWrapper.style.display = null;
+    menuButton.setAttribute("aria-expanded", "false");
+    document.querySelector("body").style.overflow = null;
+};
+
+menuButton.addEventListener("click", (event) => {
+    event.stopPropagation();
+    isMenuOpened ? hideMenu() : showMenu();
+});
+
+menuWrapper.addEventListener("click", (event) => {
+    event.stopPropagation();
+});
+
+window.addEventListener("click", () => {
+    if (isMenuOpened) hideMenu();
+});
+
+window.addEventListener("resize", () => {
+    if (isMenuOpened && window.matchMedia("(min-width: 768px)")) hideMenu();
+});
+
+window.addEventListener("keydown", (event) => {
+    if (isMenuOpened && event.key == "Escape") hideMenu();
+});
